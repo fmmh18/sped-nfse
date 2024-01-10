e6bd068c3655d43444807274985c0d8ed4cc9d4b
Flavio Hayashida <fmmh18@gmail.com>
2024-01-10 12:15:48 -0400

    correção para emissao da nfse da pref cuiaba

--- src/Models/Issnet/Factories/ConsultarLoteRps.php
+++ src/Models/Issnet/Factories/ConsultarLoteRps.php
@@ -12,13 +12,29 @@ class ConsultarLoteRps extends Factory
         $protocolo
     ) {
         $method = "ConsultarLoteRpsEnvio";
-        $xsd = 'servico_consultar_lote_rps_envio';
-        $content = $this->requestFirstPart($method, $xsd);
-        $content .= Header::render($remetenteTipoDoc, $remetenteCNPJCPF, $inscricaoMunicipal);
-        $content .= "<Protocolo>$protocolo</Protocolo>";
-        $content .= "</$method>";
+        $content = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:nfse="http://nfse.abrasf.org.br">';
+        $content .= '<soapenv:Body>';
+	    $content .= '<nfse:ConsultarLoteRps>';
+	  	$content .= '<nfseCabecMsg>';
+		$content .= '<cabecalho xmlns="http://www.abrasf.org.br/nfse.xsd" versao="2.04">';
+		$content .= '<versaoDados>2.04</versaoDados>';
+		$content .= '</cabecalho>';
+		$content .= '</nfseCabecMsg>';
+		$content .= '<nfseDadosMsg>';
+		$content .= '<ConsultarLoteRpsEnvio xmlns="http://www.abrasf.org.br/nfse.xsd">';
+		$content .= '<Prestador>';
+		$content .= '<CpfCnpj>';
+		$content .= '<Cnpj>'.$remetenteCNPJCPF.'</Cnpj>';
+		$content .= '</CpfCnpj>';
+		$content .= '<InscricaoMunicipal>'.$inscricaoMunicipal.'</InscricaoMunicipal>';
+		$content .= '</Prestador>';
+		$content .= '<Protocolo>'.$protocolo.'</Protocolo>';
+		$content .= '</ConsultarLoteRpsEnvio>';
+      	$content .= '</nfseDadosMsg>';
+	    $content .= '</nfse:ConsultarLoteRps>';
+        $content .= '</soapenv:Body>';
+        $content .= '</soapenv:Envelope>';
         $body = $this->clear($content);
-        $this->validar($versao, $body, 'Issnet', $xsd, '');
         return $body;
     }
 }
